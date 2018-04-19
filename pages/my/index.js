3a67450d293f9656cc266483aee58836a3fabf1f
BoBoooooo <17746714@qq.com>
2018-04-19 17:28:06 +0800

    修改列表界面 卡片式风格UI，调整后台API登陆代码逻辑

--- pages/my/index.js
+++ pages/my/index.js
@@ -5,7 +5,9 @@ Page({
     balance:0,
     freeze:0,
     score:0,
-    score_sign_continuous:0
+    score_sign_continuous:0,
+    hidden:true,
+    inputValue:""
   },
 	onLoad() {
     
@@ -56,6 +58,73 @@ Page({
         })
       }
     })
-  }
+  },
+  ChangePassword:function(){
+    this.setData({
+      hidden: false
+
+
+    });
+
+  },
+  cancel: function () {
+    this.setData({
+      hidden: true
+    });
+  },
+  //确认  
+  confirm: function () {
+  var that =this;
+    wx.request({
+      url: 'https://www.aicareu.com/api/login/ChangePassword',
+      data: {
+        "Password": that.data.inputValue
+      },
+      method: "post",
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        if(data.res.Success){
+          wx.removeStorage({
+            key: 'Token',
+            success: function (res) {
+
+              wx.showModal({
+                title: '提示',
+                content: '修改成功,请重新登录',
+                showCancel: false,
+                success: function (res) {
+                  if (res.confirm) {
+                    wx.redirectTo({
+                      url: "/pages/login/login"
+
+                    })
+                  }
+                }
+              })
+            }
+          })
+         
+        }
+        
+      },
+      fail: function (res) {
+        console.log(res)
+
+      }
+    });
+   
+
+    this.setData({
+      hidden: true
+    })
+  },
+  bindKeyInput:function(e){
+    this.setData({
+      inputValue: e.detail.value
+    })
+
+  }  
   
 })
